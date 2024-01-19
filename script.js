def34c2873d15f1c89bf7d7637c40ee9909dd00c
Dmytro Oliinyk <oliynykdmitry83@gmail.com>
2024-01-19 11:36:36 +0000

    stage 2

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-fetchData()
+//import fs from "fs";
+import OpenAI from "openai";
+
+const openAi = new OpenAI();
+
 
 async function fetchData(){
 
     try{
-        const response = await fetch()
+
+        const request = document.getElementById("generateImage").value.toLowerCase()
+
+        const response = await openai.createImage({
+            model: "dall-e-3",
+            prompt: `${request}`,
+            n: 1,
+            size: "1024x1024",
+          });
+          image_url = response.data.data[0].url;
+          
 
         if(!response.ok){
             throw new Error("Could not fetch the sourse");
@@ -11,8 +25,15 @@ async function fetchData(){
 
         const data = await response.json();
         console.log(data);
+        const createdImage = image_url;
+        const imgElement = document.getElementById("cretedImage");
+
+        imgElement.src = createdImage;
+        imgElement.style.display = "block";
     }
-    catch{
-        console.error(error);
+    catch(error){
+        console.error(error => console.error(error));
     }
 }
+
+fetchData()
